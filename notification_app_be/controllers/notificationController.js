00c90d6118de746cd000ce547b1fa6876c090ace
prakhar pandey <prakhar9704@gmail.com>
2026-06-09 11:37:16 +0530

    added server

---
+++ notification_app_be/controllers/notificationController.js
@@ -0,0 +1,72 @@
+import Notification
+from "../models/Notification.js";
+
+import { Log }
+from "../../logging_middleware/logger.js";
+
+export const createNotification =
+async (req, res) => {
+
+    try {
+
+        const { title, message } = req.body;
+
+        const notification =
+        await Notification.create({
+            title,
+            message
+        });
+
+        await Log(
+            "backend",
+            "info",
+            "controller",
+            "Notification created"
+        );
+
+        res.status(201).json({
+            success: true,
+            notification
+        });
+
+    }
+    catch (error) {
+
+        await Log(
+            "backend",
+            "error",
+            "controller",
+            error.message
+        );
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+};
+
+export const getNotifications =
+async (req, res) => {
+
+    try {
+
+        const notifications =
+        await Notification.find();
+
+        res.status(200).json({
+            success: true,
+            notifications
+        });
+
+    }
+    catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+};
---
+++ notification_app_be/models/notification.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const notificationSchema =
+new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+
+        message: {
+            type: String,
+            required: true
+        },
+
+        read: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Notification =
+mongoose.model(
+    "Notification",
+    notificationSchema
+);
+
+export default Notification;
